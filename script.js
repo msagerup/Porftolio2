7f10e320de58cecd909505cc859a6c38cde01c43
Magnus Sagerup <magnus.sagerup@gmail.com>
2017-06-20 22:05:44 +0200

    added new pages, and first working link

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 
 var hero = document.querySelector('.hero');
+var intro = document.querySelector('.site_content');
+
+
+var mainLinksOne = document.querySelectorAll('li')[0];
+var mainLinksTwo = document.querySelectorAll('li')[1];
+var mainLinksThree = document.querySelectorAll('li')[2];
 
 hero.addEventListener('click', function() {
   hero.classList.add('pt-page-moveToTop');
+  setTimeout(function() {
+  	hero.style.visibility = 'hidden'
+  },1000)
 });
 
 
-var mainNav = document.querySelectorAll('li');
+mainLinksOne.addEventListener('click', function(){
+	intro.style.visibility = 'hidden';
+})
 
-for (i = 0; i < mainNav.length; i++) {
-	mainNav[i].addEventListener('click', function(){
-		document.body.style.background = 'red'	
-	})
-}
+
+mainLinksOne.addEventListener('click', function(){
+	document.body.style.background = 'green'
+});
+
+mainLinksTwo.addEventListener('click', function(){
+	document.body.style.background = 'yellow'
+});
+
+mainLinksThree.addEventListener('click', function(){
+	document.body.style.background = 'red'
+});
 
 
 var myButton = document.querySelector('button');
