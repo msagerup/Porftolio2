12aa04ecc3332b2e486590389a637d6bc25cc7aa
Magnus Sagerup <magnus.sagerup@gmail.com>
2017-07-19 16:55:41 +0200

    new website consept

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
+ 
 
-var hero = document.querySelector('.hero');
-var intro = document.querySelector('.site_content');
+document.getElementsByTagName("body")[0].style.background="-webkit-linear-gradient(-90deg," + randomColorOne() + ", " + randomColorTwo() +") fixed, transparent";
 
+function randomColorOne () {
+	var r = Math.floor(Math.random() * 256); 
+	var g = Math.floor(Math.random() * 256); 
+	var b = Math.floor(Math.random() * 256); 
+	return "rgba(" + r + ", " + g + ", " + b + ",0.2 " + ")"
+}
 
-var mainLinksOne = document.querySelector('.work_link');
-var mainLinksTwo = document.querySelector('.project_link');
-var mainLinksThree = document.querySelector('.contact_link');
-
-hero.addEventListener('click', function() {
-  hero.classList.add('pt-page-moveToTop');
-  setTimeout(function() {
-  	hero.style.visibility = 'hidden'
-  },1000)
-});
-
-
-mainLinksOne.addEventListener('click', function(){
-	intro.style.visibility = 'hidden';
-})
-
-
-
-
-var myButton = document.querySelector('button');
-
-
-
-
-
-myButton.addEventListener('click', function() {
-	document.body.style.background = 'red';
-})
-
+function randomColorTwo () {
+	var r = Math.floor(Math.random() * 256); 
+	var g = Math.floor(Math.random() * 256); 
+	var b = Math.floor(Math.random() * 256); 
+	
+	return "rgba(" + r + ", " + g + ", " + b + ",0.2 " + ")"
+}
