76ec16cf69566454185ae4c726bf5a0d35d7d34b
Magnus Sagerup <magnus.sagerup@gmail.com>
2017-07-20 23:19:59 +0200

    changed left layout, added divs, and automated random background on div change

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
- 
+/*
 
-document.getElementsByTagName("body")[0].style.background="-webkit-linear-gradient(-90deg," + randomColorOne() + ", " + randomColorTwo() +") fixed, transparent";
+RANDOM BACKGROUND COLOR
 
-function randomColorOne () {
+*/ 
+
+document.getElementsByTagName("body")[0].style.background="-webkit-linear-gradient(-90deg," + randomColors() + ", " + randomColors() +") fixed, transparent";
+
+function randomColors () {
 	var r = Math.floor(Math.random() * 256); 
 	var g = Math.floor(Math.random() * 256); 
 	var b = Math.floor(Math.random() * 256); 
 	return "rgba(" + r + ", " + g + ", " + b + ",0.2 " + ")"
 }
 
-function randomColorTwo () {
-	var r = Math.floor(Math.random() * 256); 
-	var g = Math.floor(Math.random() * 256); 
-	var b = Math.floor(Math.random() * 256); 
-	
-	return "rgba(" + r + ", " + g + ", " + b + ",0.2 " + ")"
+
+$('#cv').bind('mousewheel', function(e){
+    if(e.originalEvent.wheelDelta / 120 < 0) {
+        $('#cv').hide();
+        $('#about').show();
+        setBackgroundColor()
+      
+    
+    }
+
+});
+
+$('#about').bind('mousewheel', function(e){
+    if(e.originalEvent.wheelDelta / 120 < 0) {
+        $('#about').hide();
+        $('#work').show();
+        setBackgroundColor()
+        
+    
+    } else {
+    	$('#about').hide();
+    	$('#cv').show();
+    	setBackgroundColor()
+    }
+    
+});
+
+$('#work').bind('mousewheel', function(e){
+    if(e.originalEvent.wheelDelta / 120 > 0) {
+        $('#work').hide();
+        $('#about').show();
+        setBackgroundColor()
+    }
+    
+});
+
+function setBackgroundColor () {
+	document.getElementsByTagName("body")[0].style.background="-webkit-linear-gradient(-90deg," + randomColors() + ", " + randomColors() +") fixed, transparent";
 }
